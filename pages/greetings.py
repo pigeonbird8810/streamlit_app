a70dabcbe63d50012b6eb504d485ed66df7de93b
Kawai Hayato <A>
2025-02-09 11:26:07 +0900

    修正

--- pages/greetings.py
+++ pages/greetings.py
@@ -1,3 +1,2 @@
 import streamlit as st
 st.write("これを見つけられたあなたはハッピー")
-st.image("goldtiger.jpg",caption="よく頑張ったな")
--- practice.py
+++ practice.py
@@ -5,10 +5,9 @@ full = 0
 
 num = int(st.number_input("お好きな数字を入力？",min_value=1,max_value=100,step=1))
 
-if st.button("ここをクリックして希少品をget?"):
+if st.button("push!"):
     for i in range(num):
         rundom_number = random.randint(-1000,1000)
         st.write(f"{rundom_number}万円をget!")
         full += rundom_number
     st.write(f"合計{full}万円をゲット！")
-    st.text_input("ここにメールアドレスを入力！")
